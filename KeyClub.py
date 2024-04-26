6571ddcf378a949b2caecaff3a35cf5d3f385754
Sree <Skarmoney99@gmail.com>
2024-04-25 19:34:27 -0500

    Update KeyClub.py

--- KeyClub.py
+++ KeyClub.py
@@ -341,7 +341,7 @@ def render_contact_page():
             st.markdown(f"Click [here](mailto:{preferrer_email}) to send an email to your Officer Supervisor.", unsafe_allow_html=True)
         
         if col5.button("Contact the Officer Supervisor", key="Freshman Rep"):
-            st.markdown(f"Click [here](mailto:{prefecody_email}) to send an email to your Officer Supervisor.", unsafe_allow_html=True)
+            st.markdown(f"Click [here](mailto:{prefecody_email}) to send an email to your Freshman Rep.", unsafe_allow_html=True)
             
     page_bg_img = """
     <style>
